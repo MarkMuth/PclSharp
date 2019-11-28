bc4f9ac6203991696f5dd11ec776ab08f6ff4de1
Mark Muth <mark.muth@himt.de>
2019-11-28 17:31:17 +0100

    Fix DllName to include ".dll" extension.

--- src/PclSharp/Native.cs
+++ src/PclSharp/Native.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using RIS = System.Runtime.InteropServices;
+﻿using RIS = System.Runtime.InteropServices;
 
 namespace PclSharp
 {
     public static class Native
     {
-        public const string DllName = "PclSharp.Extern";
+        public const string DllName = "PclSharp.Extern.dll";
         public const RIS.CallingConvention CallingConvention = RIS.CallingConvention.Cdecl;
 
         static Native()
